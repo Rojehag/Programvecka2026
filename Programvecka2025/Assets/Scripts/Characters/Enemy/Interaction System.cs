da3a8759d2d0ee90c9fa875ef3f5e82c069aa176
Erik-Thorin <erik.thorin.stockholm@gmail.com>
2026-01-09 12:09:58 +0100

    Påbörjat interaction med enemy och börja fightas

--- Programvecka2025/Assets/Scripts/Characters/Enemy/Interaction System.cs
+++ Programvecka2025/Assets/Scripts/Characters/Enemy/Interaction System.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class InteractionSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    bool isPlayerInRange = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F) && isPlayerInRange)
+        {
+            StartFight();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isPlayerInRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        isPlayerInRange = false;
+    }
+
+    void StartFight()
+    {
+        print("Let's start fighting!!!");
     }
 }
