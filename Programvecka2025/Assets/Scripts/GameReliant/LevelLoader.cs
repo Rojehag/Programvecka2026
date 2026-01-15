6c611afba0cbc5d4ebef42f2e69d228cce1446ef
Erik-Thorin <erik.thorin.stockholm@gmail.com>
2026-01-15 12:01:46 +0100

    idk

--- Programvecka2025/Assets/Scripts/GameReliant/LevelLoader.cs
+++ Programvecka2025/Assets/Scripts/GameReliant/LevelLoader.cs
@@ -20,7 +20,7 @@ public class LevelLoader : MonoBehaviour
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    public IEnumerator LoadLevel(int levelIndex)
     {
         transition.SetTrigger("Start");
         musicFade.FadeInMusic();
